ae071846eef4bd3f7b476f7bb22c9d85442d1b93
leocavalcante <lc@leocavalcante.com>
2020-03-12 18:07:48 -0300

    Update Gigasecond solution after mentors feedback

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,7 +1,7 @@
-use chrono::{DateTime, Utc, Duration};
+use chrono::{DateTime, Duration, Utc};
 use std::ops::Add;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start.add(Duration::seconds(1_000_000_000))
+    start + Duration::seconds(1_000_000_000)
 }
