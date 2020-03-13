fad485c7d2ee10338485efa8777bc675b9702cf8
leocavalcante <lc@leocavalcante.com>
2020-03-12 22:21:39 -0300

    Clock passing creation

---
+++ clock/src/lib.rs
@@ -0,0 +1,32 @@
+use std::fmt::{Display, Formatter, Result};
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let minutes = minutes + (hours * 60);
+        let hours = (f64::from(minutes) / 60.0).floor() as i32;
+
+        Clock {
+            hours: (24 + (hours % 24)) % 24,
+            minutes: (60 + (minutes % 60)) % 60,
+        }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock {
+            hours: self.hours,
+            minutes: self.minutes,
+        }
+    }
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
