32e280ed1925943f8d6d5ce7bc345e2703ebd966
leocavalcante <lc@leocavalcante.com>
2020-04-01 21:46:56 -0300

    Add Atbash Cipher solution

---
+++ atbash-cipher/src/lib.rs
@@ -0,0 +1,28 @@
+fn process(char: char) -> String {
+    if char.is_digit(10) {
+        char.to_string()
+    } else {
+        char::from(122 - ((char as u8) - 97)).to_string()
+    }
+}
+
+pub fn encode(plain: &str) -> String {
+    plain
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .map(process)
+        .collect::<Vec<String>>()
+        .chunks(5)
+        .map(|c| c.join(""))
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+pub fn decode(cipher: &str) -> String {
+    cipher
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .map(process)
+        .collect()
+}
